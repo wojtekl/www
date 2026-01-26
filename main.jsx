39edd3c030459e7dd9a223e4d92de518c818760a
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-26 12:59:49 +0100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,314 @@
+import { useCallback } from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
+import i18n from 'i18next'
+import { initReactI18next, useTranslation } from 'react-i18next'
+
+
+const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
+
+i18n.use(initReactI18next).init({
+  resources: resources,
+  lng: lang,
+  fallbacking: "pl",
+  interpolation: {
+    escapeValue: false
+  }
+})
+
+/* Divider */
+const Divider = () => <div style={{width: '100%', height: '3rem', backgroundColor: '#0000001a', border: 'solid rgba(0,0,0,.15)', borderWidth: '1px 0', boxShadow: 'inset 0 .5em 1.5em #0000001a,inset 0 .125em .5em #00000026'}}></div>
+
+
+/* Hero */
+const Hero = (props) => {
+  const { image, description, urlButtonOnline, small } = props
+  const navigate = useNavigate()
+  const { t } = useTranslation()
+
+  const handleClick = useCallback((name) => {
+    navigate(`${urlButtonOnline}`)
+  }, [name])
+  
+  return <div class="px-4 py-5 my-5 text-center">
+      <img class="d-block mx-auto mb-4" src={image} />
+      <h1 class="display-5 fw-bold">{t(small)}</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead m-4">{t(description)}</p>
+        <div class="d-grip gap-2 d-sm-flex justify-content-sm-center">
+          <a class="btn btn-primary btn-lg px-4 gap-3" href={urlButtonOnline}>{t('button_online')}</a>
+        </div>
+      </div>
+    </div>
+}
+
+
+/* Carousel */
+const Carousel = (props) => {
+  const { id, gallery, images } = props
+  const { t } = useTranslation()
+
+  return <div id={`carousel${id}`} class="carousel slide">
+    <div class="carousel-inner">
+      {images.map((element, index) => {
+        const caption = `${t('carousel_image')} ${index + 1}`
+        return <div class={`carousel-item ${0 === index ? 'active' : ''}`}>
+          <figure class="figure">
+            <img src={`${gallery}${id}_${element}`} class="d-block img-fluid" alt={caption} />
+            <figcaption class="figcaption">{caption}</figcaption>
+          </figure>
+        </div>
+      }
+      )}
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target={`#carousel${id}`} data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">{t('carousel_previous')}</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target={`#carousel${id}`} data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">{t('carousel_next')}</span>
+    </button>
+  </div>
+}
+
+
+/* Accordion */
+const AccordionItem = (props) => {
+  const { id, parent, show = false, children } = props
+  const { t } = useTranslation()
+
+  return <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class={`accordion-button ${!show ? 'collapsed' : ''}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${id}`} aria-expanded="true" aria-controlls={`collapse${id}`}>{t(`accordion_header_${id}`)}</button>
+    </h2>
+    <div id={`collapse${id}`} class={`accordion-collapse collapse ${!!show ? 'show' : ''}`} data-bs-parent={`#${parent}`}>
+      <div class="accordion-body">
+        <div class="row">
+          {children}
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+
+/* Howto */
+const Howto = () => {
+  const { t } = useTranslation()
+
+  const gallery = 'https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/'
+
+  return <>
+    <header>
+      <div class=" text-bg-dark collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4>{t('title_contact')}</h4>
+              <p class="text-body-secondary">{t('description_contact')}</p>
+            </div>
+           <div class="col-sm-4 offset-md-1 py-4">
+              <h4>{t('subtitle_contact')}</h4>
+               <ul class="list-unstyled">
+                <li>
+                   <a href="mailto:wleap.zhulp@slmails.com" class="text-white">{t('link_emailus')}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+          <a class="navbar-brand d-flex align-items-center">
+            <strong>{t('title_home')}</strong>
+          </a>
+          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <h1 class="text-body-emphasis">{t('header_install')}</h1>
+        <p class="fs-5 col-md-8 mb-5">{t('description_install')}</p>
+        <hr class="col-3 col-md-2 mb-5"></hr>
+        <div class="accordion" id="accordionExample">
+          <AccordionItem id="iphone" parent="accordionExample" show={true}>
+            <div class="col-md-10 col-sm-6">
+              <strong>{t('accordion_intro_iphone')}</strong>
+              <ul>
+                <li>{t('accordion_contd1_iphone')}</li>
+                <li>{t('accordion_contd2_iphone')}</li>
+                <li>{t('accordion_contd3_iphone')}</li>
+                <li>{t('accordion_contd4_iphone')}</li>
+              </ul>
+            </div>
+            <div class="col-md-2 col-sm-6" data-bs-theme="dark">
+              <Carousel id="iphone" gallery={gallery} images={['1.png', '2.png', '3.png', '4.png']} />
+            </div>
+          </AccordionItem>
+          <AccordionItem id="android" parent="accordionExample">
+            <div class="col-md-2 col-sm-6" data-bs-theme="dark">
+              <Carousel id="android" gallery={gallery} images={['1.png', '2.png', '3.png', '4.png']} />
+            </div>
+            <div class="col-md-10 col-sm-6">
+              <ul>
+                <strong>{t('accordion_intro_android')}</strong>
+                <li>{t('accordion_contd1_android')}</li>
+                <li>{t('accordion_contd2_android')}</li>
+                <li>{t('accordion_contd3_android')}</li>
+                <li>{t('accordion_contd4_android')}</li>
+              </ul>
+            </div>
+          </AccordionItem>
+          <AccordionItem id="windows" parent="accordionExample">
+            <div class="col-md-6 col-sm-6">
+              <ul>
+                <strong>{t('accordion_intro_windows')}</strong>
+                <li>{t('accordion_contd1_windows')}</li>
+                <li>{t('accordion_contd2_windows')}</li>
+                <li>{t('accordion_contd3_windows')}</li>
+                <li>{t('accordion_contd4_windows')}</li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-sm-6" data-bs-theme="dark">
+              <Carousel id="windows" gallery={gallery} images={[`1.png`, '2.png', '3.png', '4.png']} />
+            </div>
+          </AccordionItem>
+        </div>
+      </div>
+    </main>
+    <footer class="text-body-secondary py-5">
+      <div class="container">
+        <p class="float-end mb-1">
+          <a href="#">{t('button_backtotop')}</a>
+        </p>
+        <p class="mb-1">{t('description_copyright')}</p>
+        <p class="mb-0"><a href="/">{t('button_home')}</a></p>
+      </div>
+    </footer>
+  </>
+}
+
+
+/* Card */
+const Card => (props) => {
+  const { image, description, urlButtonOnline, urlButtonGet, small } = props
+  const { t } = useTranslation()
+
+  return <div class="col">
+    <div class="card shadow-sm">
+      <img src={image} style={{ padding: '3rem' }} alt={t(small)} />
+      <div class="card-body">
+        <p class="card-text">{t(description)}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <a href={urlButtonOnline} class={`btn btn-sm btn-outline-secondary ${!urlButtonOnline ? "d-none" : ""}`} role="button">{t('button_online')}</a>
+            <a href={urlButtonGet} class={`btn btn-sm btn-outline-secondary ${!urlButtonGet ? "d-none" : ""}`} role="button" rel="external">{t('button_get')}</a>
+          </div>
+          <small class="text-body-secondary">{t(small)}</small>
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+
+/* App */
+const App = () => {
+  const { t } = useTranslation()
+
+  return <>
+    <header>
+      <div class=" text-bg-dark collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4>{t('title_contact')}</h4>
+              <p class="text-body-secondary">{t('description_contact')}</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4>{t('subtitle_contact')}</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="mailto:wleap.zhulp@slmails.com" class="text-white">{t('link_emailus')}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+          <a class="navbar-brand d-flex align-items-center">
+            <strong>{t('title_home')}</strong>
+          </a>
+          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">{t('title_products')}</h1>
+            <p class="lead text-body-secondary">{t('description_products')}</p>
+            <p>
+              <a href="/howto/" class="btn btn-primary my-2" role="button" rel="external">{t('button_howto')}</a>
+              <a href="" class="btn btn-secondary my-2 d-none" role="button" rel="external">{t('')}</a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <Divider />
+      <Hero
+        image="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/pricey/Pricey/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp"
+        description="description_pricey"
+        urlButtonOnline="https://cennik.wlap.pl/"
+        small="name_pricey"
+      />
+      <Divider />
+      <Hero
+        image="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MojaParafia/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp"
+        description="description_myparish"
+        urlButtonOnline="https://parafia.wlap.pl/"
+        small="name_myparish"
+      />
+      <Divider />
+    </main>
+
+    <footer class="text-body-secondary py-5">
+      <div class="container">
+        <p class="float-end mb-1">
+          <a href="#">{t('button_backtotop')}</a>
+        </p>
+        <p class="mb-1">{t('description_copyright')}</p>
+        <p class="mb-0">{t('description_guide')}</p>
+      </div>
+    </footer>
+  </>
+}
+
+
+const container = document.getElementById('app')
+
+const root = ReactDOM.createRoot(container)
+
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/howto" element={<Howto />} />
+    </Routes>
+  </Router>
+)
