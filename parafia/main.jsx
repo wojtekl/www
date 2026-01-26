a223448c3056ff7800b4fa7a2c2b3a50411238dc
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-26 16:02:15 +0100

    Update main.jsx

--- parafia/main.jsx
+++ parafia/main.jsx
@@ -1456,7 +1456,7 @@ const App = () => {
 
 const container = document.getElementById('root')
 
-const root = ReactDOM.createRoot(container)
+const root = createRoot(container)
 
 root.render(<Provider store={store}>
   <Router>
