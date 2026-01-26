f2a9d7581e588277a4c6bb4a031f4691b92d9585
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-26 14:58:05 +0100

    Create sw.js

---
+++ parafia/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "myparish-v2";
+
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if ("GET" === event.request.method && !/api/.test(url.pathname)) {
+    event.respondWith(networkFirst(event.request));
+  }
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
