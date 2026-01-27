52f7569feab067b3471ba11c221fcbc1ee2d2edf
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-27 16:09:29 +0100

    Create sw.js

---
+++ cennik/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "pricey-v2";
+
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if ("GET" === event.request.method) {
+    event.respondWith(networkFirst(event.request));
+  }
+});
