1c58573a9464bb836cb15a192eece4d982ef89b0
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-30 16:35:58 +0100

    Update repository.php

--- cennik/api/repository.php
+++ cennik/api/repository.php
@@ -39,25 +39,26 @@ class Repository {
     $statement -> bindParam(":coupon", $coupon);
     $statement -> bindParam(":bulk", $bulk);
     $statement -> bindParam(":day", $day);
-    return $this -> execute($statement)
+    return $this -> execute($statement);
   }
   
   public function getItem($item) {
     $statement = $this -> sql -> prepare("SELECT `ID`, `SKLEP`, `CENA`, `DODANO`, `COUPON`, `BULK` FROM `CENA` WHERE `PRODUKT` = :item ORDER BY `DODANO` DESC, `CENA`, `SKLEP` LIMIT 10");
     $statement -> bindParam(":item", $item);
-    return $this -> execute($statement)
+    return $this -> execute($statement);
   }
 
   public function getSelected($selected) {
     $statement = $this -> sql -> prepare("SELECT `ID`, `PRODUKT`, `SKLEP`, `CENA`, `DODANO`, `COUPON`, `BULK`, (SELECT MIN(`CENA`) FROM `CENA` WHERE `PRODUKT` = `c`.`PRODUKT`) AS `LOWEST` FROM `CENA` `c` WHERE `ID` IN (:selected) ORDER BY `SKLEP`, `PRODUKT`");
     $statement -> bindParam(":selected", $selected);
-    return $this -> execute($statement)
+    return $this -> execute($statement);
   }
   
   public function getItems($country) {
-    $statement = $this -> sql -> prepare("SELECT COALESCE(`q1`.`ID`, `q2`.`ID`) AS `ID`, `q2`.`PRODUKT` AS `PRODUKT`, COALESCE(`q1`.`SKLEP`, `q2`.`SKLEP`) AS `SKLEP`, COALESCE(CONVERT(`q1`.`CENA`, CHAR), CONVERT(`q2`.`CENA`, CHAR)) AS `CENA`, COALESCE(`q1`.`DODANO`, `q2`.`DODANO`) AS `DODANO`, COALESCE(`q1`.`COUPON`, `q2`.`COUPON`) AS `COUPON`, COALESCE(`q1`.`BULK`, `q2`.`BULK`) AS `BULK`, IFNULL(CONVERT(`q3`.`CENA`, CHAR), 0) AS `LOWEST` FROM (SELECT `ID`, `PRODUKT`, `SKLEP`, `CENA`, `DODANO`, `COUPON`, `BULK` FROM `CENA` `c1` WHERE `ID` = (SELECT `ID` FROM `CENA` WHERE `PRODUKT` = `c1`.`PRODUKT` AND `KRAJ` LIKE '%:country%' AND `DODANO` >= UTC_TIMESTAMP - INTERVAL 7 DAY ORDER BY `CENA`, `DODANO` DESC LIMIT 1)) `q1` RIGHT JOIN (SELECT `ID`, `PRODUKT`, `SKLEP`, `CENA`, `DODANO`, `COUPON`, `BULK` FROM `CENA` `c2` WHERE `ID` = (SELECT `ID` FROM `CENA` WHERE `PRODUKT` = `c2`.`PRODUKT` AND `KRAJ` LIKE '%:country%' ORDER BY `DODANO` DESC LIMIT 1)) `q2` ON `q2`.`PRODUKT` = `q1`.`PRODUKT` LEFT JOIN (SELECT `ID`, `CENA` FROM `CENA` `c2` WHERE `ID` = (SELECT `ID` FROM `CENA` WHERE `PRODUKT` = `c2`.`PRODUKT` AND `KRAJ` LIKE '%:country%' ORDER BY `CENA`, `DODANO` DESC LIMIT 1)) `q3` ON `q3`.`ID` = `q1`.`ID` ORDER BY `DODANO` DESC");
-    $statement -> bindParam(":country", $country);
-    return $this -> execute($statement)
+    $statement = $this -> sql -> prepare("SELECT COALESCE(`q1`.`ID`, `q2`.`ID`) AS `ID`, `q2`.`PRODUKT` AS `PRODUKT`, COALESCE(`q1`.`SKLEP`, `q2`.`SKLEP`) AS `SKLEP`, COALESCE(CONVERT(`q1`.`CENA`, CHAR), CONVERT(`q2`.`CENA`, CHAR)) AS `CENA`, COALESCE(`q1`.`DODANO`, `q2`.`DODANO`) AS `DODANO`, COALESCE(`q1`.`COUPON`, `q2`.`COUPON`) AS `COUPON`, COALESCE(`q1`.`BULK`, `q2`.`BULK`) AS `BULK`, IFNULL(CONVERT(`q3`.`CENA`, CHAR), 0) AS `LOWEST` FROM (SELECT `ID`, `PRODUKT`, `SKLEP`, `CENA`, `DODANO`, `COUPON`, `BULK` FROM `CENA` `c1` WHERE `ID` = (SELECT `ID` FROM `CENA` WHERE `PRODUKT` = `c1`.`PRODUKT` AND `KRAJ` LIKE :country AND `DODANO` >= UTC_TIMESTAMP - INTERVAL 7 DAY ORDER BY `CENA`, `DODANO` DESC LIMIT 1)) `q1` RIGHT JOIN (SELECT `ID`, `PRODUKT`, `SKLEP`, `CENA`, `DODANO`, `COUPON`, `BULK` FROM `CENA` `c2` WHERE `ID` = (SELECT `ID` FROM `CENA` WHERE `PRODUKT` = `c2`.`PRODUKT` AND `KRAJ` LIKE :country ORDER BY `DODANO` DESC LIMIT 1)) `q2` ON `q2`.`PRODUKT` = `q1`.`PRODUKT` LEFT JOIN (SELECT `ID`, `CENA` FROM `CENA` `c2` WHERE `ID` = (SELECT `ID` FROM `CENA` WHERE `PRODUKT` = `c2`.`PRODUKT` AND `KRAJ` LIKE :country ORDER BY `CENA`, `DODANO` DESC LIMIT 1)) `q3` ON `q3`.`ID` = `q1`.`ID` ORDER BY `DODANO` DESC");
+    $likeCountry = '%' . $country . '%';
+    $statement -> bindParam(":country", $likeCountry);
+    return $this -> execute($statement);
   }
 
   public function createVisit($address, $client, $country) {
@@ -65,7 +66,7 @@ class Repository {
     $statement -> bindParam(":address", $address);
     $statement -> bindParam(":client", $client);
     $statement -> bindParam(":country", $country);
-    return $this -> execute($statement)
+    return $this -> execute($statement);
   }
 }
 
