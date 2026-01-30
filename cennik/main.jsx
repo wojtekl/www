ef1bc3c3cfc9631a58678b985c4540855439a448
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-30 17:44:44 +0100

    Update main.jsx

--- cennik/main.jsx
+++ cennik/main.jsx
@@ -193,7 +193,7 @@ const List = (props) => {
             {(!detailsPage ? filtered : list).map(row => {
               const enabled = select === row['item']
               return (<tr onMouseOver={() => setSelect(!detailsPage ? row['item'] : row['id'])}>
-                <td><input type="checkbox" class="form-check-input" name="selected" checked={() => saved.includes(row['id'])} onChange={handleChange} aria-label={t('label_select')} /></td>
+                <td><input type="checkbox" class="form-check-input" name="selected" checked={saved.includes(row['id'])} onChange={handleChange} aria-label={t('label_select')} /></td>
                   {properties.map(property => {
                     if ('posted' === property) {
                       return <td><DateFormatter timestamp={row[property]} locale={locale} /></td>
