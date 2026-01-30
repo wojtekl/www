718581d22ae392a72e8771eba83b334040dedc81
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-30 17:43:23 +0100

    Update items.php

--- cennik/api/items.php
+++ cennik/api/items.php
@@ -1,64 +1,52 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Headers: Content-Type");
+  header("Content-Type: application/json");
 
-require "./repository.php";
+  require "./repository.php";
 
-$country = "pl";
-$httpAcceptLanguage = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-if (isset($httpAcceptLanguage[0])) {
-  $country = strtolower(substr(trim($httpAcceptLanguage[0]), 3));
-}
+  $country = "pl";
+  $httpAcceptLanguage = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+  if (isset($httpAcceptLanguage[0])) {
+    $country = strtolower(substr(trim($httpAcceptLanguage[0]), 3));
+  }
 
-switch (strtolower(trim($_SERVER["REQUEST_METHOD"]))) {
-  case "post":
-    post();
-    break;
-  case "put":
-    put();
-    break;
-  case "delete":
-    delete();
-    break;
-  default:
-    get();
-}
+  switch (strtolower(trim($_SERVER["REQUEST_METHOD"]))) {
+    case "get":
+      get($repository, $country);
+      break;
+    case "post":
+      pot();
+      break;
+    default:
+      pot();
+  }
 
-function get() {
-  global $country, $repository;
+  function get($repository, $country) {
   
-  if(isset($_GET["lang"])) {
-    $country = strtolower(trim($_GET["lang"]));
-  }
+    if(isset($_GET["lang"])) {
+      $country = strtolower(trim($_GET["lang"]));
+    }
   
-  $selected = trim($_GET["selected"]);
-  $result = [];
-  if ("" != $selected) {
-    $result = $repository -> getSelected($selected);
+    $selected = trim($_GET["selected"]);
+    $result = [];
+    if ("" != $selected) {
+      $result = $repository -> getSelected($selected);
+    }
+    else {
+      $result = $repository -> getItems($country);
+    }
+    $list = "[";
+    foreach ($result as $row) {
+      $list .= "{\"item\": \"${row["PRODUKT"]}\", \"store\": \"${row["SKLEP"]}\", \"price\": ${row["CENA"]}, \"posted\": \"${row["DODANO"]}\", \"coupon\": \"${row["COUPON"]}\", \"bulk\": \"${row["BULK"]}\", \"id\": ${row["ID"]}, \"lowest\": ${row["LOWEST"]}},";
+    }
+    $list .= "]";
+    echo str_replace(",]", "]", $list);
   }
-  else {
-    $result = $repository -> getItems($country);
+
+  function pot() {
+    echo("gotcha!");
   }
-  $list = "[";
-  foreach ($result as $row) {
-    $list .= "{\"item\": \"${row["PRODUKT"]}\", \"store\": \"${row["SKLEP"]}\", \"price\": ${row["CENA"]}, \"posted\": \"${row["DODANO"]}\", \"coupon\": \"${row["COUPON"]}\", \"bulk\": \"${row["BULK"]}\", \"id\": ${row["ID"]}, \"lowest\": ${row["LOWEST"]}},";
-  }
-  $list .= "]";
-  echo str_replace(",]", "]", $list);
-}
-
-function post() {
-  global $country, $repository;
-}
-
-function put() {
-  global $country, $repository;
-}
-
-function delete() {
-  global $country, $repository;
-}
 
 ?>
