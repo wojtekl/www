f7609b2d542671aaa8b51aa98e97cac49405713a
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-01 15:34:21 +0100

    Update sw.js

--- cennik/sw.js
+++ cennik/sw.js
@@ -20,3 +20,18 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(networkFirst(event.request));
   }
 });
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
