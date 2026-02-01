6bdd2d98da28751ee1105c44de54fe0eec83889c
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-01 15:21:13 +0100

    Update statistics.php

--- cennik/api/statistics.php
+++ cennik/api/statistics.php
@@ -1,39 +1,46 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+  header("Content-Type: application/json");
 
-require "./repository.php";
+  require "./repository.php";
 
-$country = "pl";
-$httpAcceptLanguage = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-if (isset($httpAcceptLanguage[0])) {
-  $country = strtolower(substr(trim($httpAcceptLanguage[0]), 3));
-}
-
-if(isset($_GET["lang"])) {
-  $country = strtolower(trim($_GET["lang"]));
-}
-
-switch (strtolower(trim($_SERVER["REQUEST_METHOD"]))) {
-  case "post":
-    break;
-  case "put":
-    break;
-  case "delete":
-    break;
-  default:
-    get();
-}
-
-function get() {
-  global $country, $repository;
-  $address = trim($_SERVER['REMOTE_ADDR']);
-  $client = trim($_SERVER['HTTP_USER_AGENT']);
-  if (isset($address) && isset($client)) {
-    $repository -> createVisit($address, $client, $country);
+  $country = "pl";
+  $httpAcceptLanguage = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+  if (isset($httpAcceptLanguage[0])) {
+    $country = strtolower(substr(trim($httpAcceptLanguage[0]), 3));
   }
-}
+
+  if(isset($_GET["lang"])) {
+    $country = strtolower(trim($_GET["lang"]));
+  }
+
+  switch (strtolower(trim($_SERVER["REQUEST_METHOD"]))) {
+    case "get":
+      get($repository, $country);
+      break;
+    case "post":
+      post($repository, $country);
+      break;
+    default:
+      pot();
+  }
+
+  function get($repository, $country) {
+    $result = $repository -> getVisit($country);
+    echo($result);
+  }
+  
+  function post($repository, $country) {
+    $address = trim($_SERVER['REMOTE_ADDR']);
+    $client = trim($_SERVER['HTTP_USER_AGENT']);
+    if (isset($address) && isset($client)) {
+      $result = $repository -> createVisit($address, $client, $country);
+      echo($result);
+    }
+  }
+
+  function pot() {
+    echo("gotcha!");
+  }
 
 ?>
