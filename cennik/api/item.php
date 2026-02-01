07ee4714fc5a65a739c78afa630145da2dc2b633
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-01 15:09:09 +0100

    Update item.php

--- cennik/api/item.php
+++ cennik/api/item.php
@@ -1,7 +1,4 @@
 <?php
-
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: Content-Type");
   header("Content-Type: application/json");
 
   require "./repository.php";
