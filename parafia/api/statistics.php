74c2bc8c0ffb8719a24c39f1985baac8a424f13a
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-02 11:27:07 +0100

    Update statistics.php

--- parafia/api/statistics.php
+++ parafia/api/statistics.php
@@ -4,15 +4,8 @@
 
   require "./repository.php";
 
-  $country = "pl";
-  $httpAcceptLanguage = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-  if (isset($httpAcceptLanguage[0])) {
-    $country = strtolower(substr(trim($httpAcceptLanguage[0]), 3));
-  }
-
-  if(isset($_GET["lang"])) {
-    $country = strtolower(trim($_GET["lang"]));
-  }
+  $httpLang = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"])[0];
+  $country = strtolower(trim($_GET["lang"] ?? isset($httpLang) ? substr($httpLang, strpos($httpLang, '-')) : 'pl'));
 
   switch (strtolower(trim($_SERVER["REQUEST_METHOD"]))) {
     case "get":
@@ -26,18 +19,15 @@
   }
 
   function get($repository) {
-    //$result = $repository -> readLog();
-    //$row = $result[0];
-    //$list = "{\"address\": \"${row["ADDRESS"]}\",\"client\": \"${row["CLIENT"]}\",\"country\": \"${row["COUNTRY"]}\",\"created\": \"${row["CREATED"]}\"}";
-    //echo($list);
-    echo $_SERVER["HTTP_ACCEPT_LANGUAGE"];
-    echo explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+    $result = $repository -> readLog();
+    $row = $result[0];
+    $list = "{\"address\": \"${row["ADDRESS"]}\",\"client\": \"${row["CLIENT"]}\",\"country\": \"${row["COUNTRY"]}\",\"created\": \"${row["CREATED"]}\"}";
+    echo($list);
   }
   
-  function post($repository) {
+  function post($repository, $country) {
     $address = trim($_SERVER['REMOTE_ADDR']);
     $client = trim($_SERVER['HTTP_USER_AGENT']);
-    $country = trim($httpAcceptLanguage[0]), 3);
     if (isset($address) && isset($client)) {
       $result = $repository -> createLog($address, $client, $country);
       echo($result);
