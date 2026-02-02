67776d07e1a8a31a87ccbf66fa9191d3b86e02e1
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-02 12:56:24 +0100

    Update statistics.php

--- cennik/api/statistics.php
+++ cennik/api/statistics.php
@@ -1,48 +1,34 @@
 <?php
-
   header("Content-Type: application/json");
 
   require "./repository.php";
 
-  $country = "pl";
-  $httpAcceptLanguage = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-  if (isset($httpAcceptLanguage[0])) {
-    $country = strtolower(substr(trim($httpAcceptLanguage[0]), 3));
-  }
-
-  if(isset($_GET["lang"])) {
-    $country = strtolower(trim($_GET["lang"]));
-  }
+  $httpLang = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"])[0];
+  $country = strtolower(trim($_GET["lang"] ?? isset($httpLang) ? substr($httpLang, strpos($httpLang, '-') + 1) : 'pl'));
 
   switch (strtolower(trim($_SERVER["REQUEST_METHOD"]))) {
     case "get":
       get($repository, $country);
-      break;
-    case "post":
-      post($repository, $country);
       break;
     default:
       pot();
   }
 
   function get($repository, $country) {
-    $result = $repository -> getVisit($country);
+    $result = $repository -> readLog($country);
     $row = $result[0];
-    $list = "{\"address\": \"${row["ADDRESS"]}\",\"client\": \"${row["CLIENT"]}\",\"country\": \"${row["COUNTRY"]}\",\"created\": \"${row["CREATED"]}\"}";
-    echo($list);
-  }
-  
-  function post($repository, $country) {
+    $list = "{\"count\": \"${row["COUNT"]}\"}";
+
     $address = trim($_SERVER['REMOTE_ADDR']);
     $client = trim($_SERVER['HTTP_USER_AGENT']);
     if (isset($address) && isset($client)) {
-      $result = $repository -> createVisit($address, $client, $country);
-      echo($result);
+      $result = $repository -> createLog($address, $client, $country);
     }
+    
+    echo($list);
   }
 
   function pot() {
     echo("gotcha!");
   }
-
 ?>
