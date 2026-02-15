fa283e62a8ff44a2c7338a520759d922b235a075
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-15 16:27:18 +0100

    Create test.jsx

---
+++ test.jsx
@@ -0,0 +1,26 @@
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`v ${title}`)
+  } catch (error) {
+    console.log(`x ${title}`)
+    console.log(error)
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+    toEqual(expected) {}
+  }
+}
+
+test('number formatter', () => {
+  const result = <NumberFormatter value="6.9" locale={locale} />
+  const expected = '6.90'
+  expect(result).toBe(expected)
+})
