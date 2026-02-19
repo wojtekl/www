dde59e9ef5b8e6b0270afb91ee21c24311c6593e
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-19 16:15:47 +0100

    Update utils.jsx

--- utils.jsx
+++ utils.jsx
@@ -2,7 +2,7 @@ const getUrlParams = () => new URLSearchParams(new URL(window.location).search)
 const getUrlParam = (param: String) => getUrlParams().get(param) ?? undefined
 const datePart = (d = new Date()) => d.toISOString().split('T')[0]
 
-const getWeeks = () => {
+const getWeeks = (months) => {
   const currentYear = new Date()
   currentYear.setHours(0, 0, 0, 0)
   currentYear.setMonth(0)
