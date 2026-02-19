886946de898c6a9ee8dc63e7a030c8c7a7ede31e
wlap.pl <wojtekl@users.noreply.github.com>
2026-02-19 13:03:50 +0100

    Update utils.jsx

--- utils.jsx
+++ utils.jsx
@@ -22,3 +22,59 @@ const NumberFormatter = (props: { value: Number, locale: String }) => {
   const format = { maximumFractionDigits: 2, minimumFractionDigits: 2 }
   return value.toLocaleString(locale, format)
 }
+
+
+/* FormInput */
+const FormInput = (props) => {
+  const { name, label, help, modalId } = props
+  
+  return <div class="form-group">
+  <label for={`${modalId}Input${name}`}>{label}</label>
+  <input type="text" class="form-control" id={`${modalId}Input${name}`} aria-describedby={`${modalId}Help${name}`} name={name} />
+  <small id={`${modalId}Help${name}`} class="form-text text-muted">{help}</small>
+</div>
+}
+
+
+/* InputText */
+const InputText = (props) => {
+  const { name, label, help, parentId } = props
+  
+  return <div class="mb-3">
+  <label for={`$parentId}${name}`} class="form-label">{label}</label>
+  <input type="text" id={`${parentId}${name}`} class="form-control" placeholder={help} name={name} />
+</div>
+}
+
+
+/* ModalForm */
+const ModalForm = (props) => {
+  const { id, title, onSubmit, label_close, label_cancel, label_save, children } = props
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.debug('handleSubmit')
+    onSubmit()
+    
+    event.stopPropagation()
+  }
+  
+  return <div class="modal fade" id={id} tabindex="-1" aria-labelledby={`title_${id}`} aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id={`title_${id}`}>{title}</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label={label_close}></button>
+        </div>
+        <div class="modal-body">
+          <form class="dane" id={`form_${id}`} enctype="multipart/form-data">{children}</form>
+        </div>
+        <div class="modal-footer">
+          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">{label_cancel}</button>
+          <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" onClick={handleSubmit}>{label_save}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+}
