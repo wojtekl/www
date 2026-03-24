f1d5dc8989e8e76a4983eabcf9581cdfd0c1acf2
wlap.pl <wojtekl@users.noreply.github.com>
2026-03-24 15:54:05 +0100

    Update repository.php

--- grafik/api/repository.php
+++ grafik/api/repository.php
@@ -134,7 +134,7 @@ class Repository {
   }
   
   public function readSettings($tenant) {
-    $statement = $this -> sql -> prepare("SELECT `ID`, `SCHEDULE`, `SHOWVISITS`, `SHOWBOOKING`, `GROUPPASSWORD` FROM `SETTINGS` WHERE `TENANT` = :tenant");
+    $statement = $this -> sql -> prepare("SELECT `ID`, `SCHEDULE`, `SHOWVISITS`, `SHOWBOOKING` FROM `SETTINGS` WHERE `TENANT` = :tenant");
     
     $statement -> bindParam(":tenant", $tenant);
     
@@ -142,12 +142,30 @@ class Repository {
   }
   
   public function updateSettings($schedule, $showVisits, $showBooking, $tenant) {
-    $statement = $this -> sql -> prepare("UPDATE `SETTINGS` SET `SCHEDULE` = :schedule, `SHOWVISITS` = :showVisits, `SHOWBOOKING` = :showBooking, `GROUPPASSWORD` = :groupPassword WHERE `TENANT` = :tenant");
+    $statement = $this -> sql -> prepare("UPDATE `SETTINGS` SET `SCHEDULE` = :schedule, `SHOWVISITS` = :showVisits, `SHOWBOOKING` = :showBooking WHERE `TENANT` = :tenant");
     
     $statement -> bindParam(":schedule", $schedule);
     $statement -> bindParam(":showVisits", $showVisits);
     $statement -> bindParam(":showBooking", $showBooking);
-    $statement -> bindParam(":groupPassword", $groupPassword);
+    $statement -> bindParam(":tenant", $tenant);
+    
+    return $this -> execute($statement);
+  }
+  
+  
+  public function readGroupPassword($tenant) {
+    $statement = $this -> sql -> prepare("SELECT `PASSWORD` FROM `SETTINGS` WHERE `TENANT` = :tenant");
+    
+    $statement -> bindParam(":tenant", $tenant);
+    
+    return $this -> execute($statement);
+  }
+  
+  public function updateGroupPassword($newHash, $address, $tenant) {
+    $statement = $this -> sql -> prepare("UPDATE `SETTINGS` SET `GROUPPASSWORD` = :hash, `ADDRESS` = :address WHERE `TENANT` = :tenant");
+    
+    $statement -> bindParam(":hash", $newHash);
+    $statement -> bindParam(":address", $address);
     $statement -> bindParam(":tenant", $tenant);
     
     return $this -> execute($statement);
